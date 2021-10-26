97c7d8b71c415012cda4155bf36b65aa6c1133fa
Nilwan de Jesus Grisosti <nilwandjg@yahoo.com.br>
2021-10-26 17:42:05 -0300

    Versão 1

---
+++ ApiWeb_Produto_Estoque_CeA/Controllers/EstoqueController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiWeb_Produto_Estoque_CeA.Models;
+
+namespace ApiWeb_Produto_Estoque_CeA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstoqueController : ControllerBase
+    {
+        private readonly EstoqueContext _context;
+
+        public EstoqueController(EstoqueContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Estoque
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Estoque>>> GetEstoqueItems()
+        {
+            return await _context.EstoqueItems.ToListAsync();
+        }
+
+        // GET: api/Estoque/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Estoque>> GetEstoque(int id)
+        {
+            var estoque = await _context.EstoqueItems.FindAsync(id);
+
+            if (estoque == null)
+            {
+                return NotFound();
+            }
+
+            return estoque;
+        }
+
+        // PUT: api/Estoque/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEstoque(int id, Estoque estoque)
+        {
+            if (id != estoque.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(estoque).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EstoqueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Estoque
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Estoque>> PostEstoque(Estoque estoque)
+        {
+            _context.EstoqueItems.Add(estoque);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEstoque", new { id = estoque.ID }, estoque);
+        }
+
+        // DELETE: api/Estoque/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEstoque(int id)
+        {
+            var estoque = await _context.EstoqueItems.FindAsync(id);
+            if (estoque == null)
+            {
+                return NotFound();
+            }
+
+            _context.EstoqueItems.Remove(estoque);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EstoqueExists(int id)
+        {
+            return _context.EstoqueItems.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ApiWeb_Produto_Estoque_CeA/Models/EstoqueContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWeb_Produto_Estoque_CeA.Models
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Estoque> EstoqueItems { get; set; }
+
+    }
+}
